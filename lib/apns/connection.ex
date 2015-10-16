18e9beee2ba7ccf9913c3a12d0c5d70eec6a5c81
Jonas Thiel <jt@neopoly.de>
2015-10-16 15:45:11 +0200

    Cleanup connection module

--- lib/apns/connection.ex
+++ lib/apns/connection.ex
@@ -1,34 +1,71 @@
 defmodule APNSx.Connection do
+  use GenServer
+  require Logger
 
-  def start do
+  def start(host, port, options) do
+    {:ok, pid} = GenServer.start_link(__MODULE__, :ok)
+    :ok = GenServer.call(pid, {:connect, host, port, options})
+    {:ok, pid}
+  end
+
+  def push(connection, message) do
+    GenServer.call(connection, {:send, message})
+  end
+
+  def close(connection) do
+    GenServer.call(connection, :close)
+  end
+
+  def handle_call({:connect, host, port, options}, _, _) do
+    Logger.info("Connecting to: #{host}:#{port}")
+    Logger.debug(inspect(options))
+
     :ssl.start()
+    {:ok, socket} = ssl_connect(host, port, options)
+    :ok = ssl_accept(socket)
+    {:reply, :ok, socket}
   end
 
-  def open(host, port, options) do
-    case ssl_connect(host, port, options) do
-      {:ok, socket} ->
-        :ssl.ssl_accept(socket)
-      {:error, reason} ->
-        {:error, reason}
+  def handle_call({:send, message}, _, socket) do
+    Logger.debug("Sending: #{inspect(message)}")
+    :ok = ssl_send(socket, message)
+    {:reply, :ok, socket}
+  end
+
+  def handle_call(:close, socket) do
+    Logger.debug("Closing connection")
+    :ok = ssl_close(socket)
+    {:reply, :ok, socket}
+  end
+
+  def handle_info({:ssl_closed, _}, socket) do
+    Logger.info("Connection closed")
+    {:stop, :normal, []}
+  end
+
+  def handle_info({:ssl, s, d}, socket) do
+    Logger.debug("Received: #{inspect(s)}, #{inspect(d)}")
+    {:noreply, socket}
+  end
+
+  def handle_info(msg, socket) do
+    Logger.warn("Unknown info: #{inspect(msg)}")
+    {:noreplay, socket}
+  end
+
+  defp ssl_send(socket, message) do
+    :ssl.send(socket, message)
+  end
+
+  defp ssl_options(options) do
+    opts = case Keyword.get(options, :sandbox) do
+      # Force TLS v1.1 as a workaround for an OTP SSL bug
+      # See
+      # * https://github.com/inaka/apns4erl/pull/65
+      # * http://erlang.org/pipermail/erlang-questions/2015-June/084935.html
+      true -> [{ :versions, [String.to_atom("tlsv1.1")] }]
+      _ -> []
     end
-  end
-
-  def send(socket, message) do
-    :ok = :ssl.send(message)
-    {:ok, socket}
-  end
-
-  def close(socket) do
-    :ok = :ssl.close(socket)
-    :ok
-  end
-
-  def ssl_options(options) do
-    # Force TLS v1.1 as a workaround for an OTP SSL bug
-    # See
-    # * https://github.com/inaka/apns4erl/pull/65
-    # * http://erlang.org/pipermail/erlang-questions/2015-June/084935.html
-    opts = [{ :versions, [String.to_atom("tlsv1.1")] }]
 
     opts = case Keyword.get(options, :cert) do
       [path: path] ->
@@ -61,7 +98,15 @@ defmodule APNSx.Connection do
     opts
   end
 
+  defp ssl_accept(socket) do
+    :ssl.ssl_accept(socket)
+  end
+
   defp ssl_connect(host, port, options) do
     :ssl.connect(String.to_char_list(host), port, ssl_options(options), options[:timeout] || :infinity)
+  end
+
+  defp ssl_close(socket) do
+    :ssl.close(socket)
   end
 end
